0494c9f10fa4e093ff893a8fa24ac254359ba52b
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-06-14 02:02:33 +0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include "snowman.cpp"
+#include <iostream>
+
+
+
+
+
+int main()
+{    
+    std::cout << ariel::snowman(11111111) << std::endl;
+    std::cout << ariel::snowman(22222222) << std::endl;
+    std::cout << ariel::snowman(12321412) << std::endl;
+    std::cout << ariel::snowman(24132424) << std::endl;
+    std::cout << ariel::snowman(12121212) << std::endl;
+    std::cout << ariel::snowman(33333333) << std::endl;
+    std::cout << ariel::snowman(44444444) << std::endl;
+}
